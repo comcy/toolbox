b0dab3e7a91132b8d04a9d99b6b4b2b420a25e90
Martin Riedel <donmarten@googlemail.com>
2020-11-21 14:14:15 +0100

    Add gRPC server to server.py and serve it on localhost:50051

--- core/server.py
+++ core/server.py
@@ -1,7 +1,13 @@
-import sys
+import asyncio
 import json
+import logging
+
+import grpc
 import requests
 from configparser import ConfigParser
+
+import storage_pb2
+import storage_pb2_grpc
 
 parser = ConfigParser()
 parser.read('config.ini')
@@ -9,6 +15,28 @@ parser.read('config.ini')
 API_URL = parser.get('cq', 'api_url')
 AUTH_TOKEN = parser.get('cq', 'auth_token')
 HEADERS = {"Authorization": "Bearer " + AUTH_TOKEN}
+
+
+class StorageService(storage_pb2_grpc.StorageServicer):
+    async def GetStorage(self, request, context):
+        storage = get_storages(request.uuid)
+        return storage_pb2.StorageResponse(name=storage['name'])
+
+
+def get_storages(uuid):
+    api_storage_url = API_URL + 'storages/%s' % uuid
+    r = requests.get(api_storage_url, headers=HEADERS)
+    return r.json()
+
+
+async def serve():
+    server = grpc.aio.server()
+    storage_pb2_grpc.add_StorageServicer_to_server(StorageService(), server)
+    listen_addr = '[::]:50051'
+    server.add_insecure_port(listen_addr)
+    logging.info("Starting server on %s", listen_addr)
+    await server.start()
+    await server.wait_for_termination()
 
 
 def main():
@@ -21,4 +49,5 @@ def main():
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(serve())
